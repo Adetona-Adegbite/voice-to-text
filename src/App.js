236e0483ebb234601778cf4a62f9ecefbef21115
Adetona <adetona67@gmail.com>
2023-08-30 02:48:04 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import { BiMicrophone } from "react-icons/bi";
+import { BsPauseCircle } from "react-icons/bs";
+import { AiOutlinePlayCircle } from "react-icons/ai";
+import { GrPowerReset } from "react-icons/gr";
 function App() {
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+  if (!browserSupportsSpeechRecognition) {
+    return <div>Your Browser Doesn't Support This App</div>;
+  }
+  const startListening = () => {
+    SpeechRecognition.startListening({ continuous: true });
+  };
+  const micColor = listening ? "white" : "black";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <div className="header">
+        <h1>Voice to Text</h1>
+      </div>
+      <div className="actions">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {listening ? "Recording..." : "Click the Record Button to Record"}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className="buttons">
+          <button
+            style={{ background: listening ? "red" : "white" }}
+            onClick={startListening}
+          >
+            <BiMicrophone color={micColor} />
+          </button>
+          <button onClick={SpeechRecognition.stopListening}>
+            {listening ? <BsPauseCircle /> : <AiOutlinePlayCircle />}
+          </button>
+          <button onClick={resetTranscript}>
+            <GrPowerReset />
+          </button>
+        </div>
+      </div>
+      <div className="data">{transcript}</div>
+      <footer>
+        <p>Made with React</p>
+      </footer>
+    </>
   );
 }
 
